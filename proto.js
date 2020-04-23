917515ffd732ed0bf0e2acefe356a633740c36ec
ahkeem1 <61710306+ahkeem1@users.noreply.github.com>
2020-04-23 12:16:26 -0500

    update

--- proto.js
+++ proto.js
@@ -5,22 +5,26 @@ var step1 = document.getElementById("step1");
 var step2 = document.getElementById("step2");
 var step3 = document.getElementById("step3");
 var step4 = document.getElementById("step4");
+var userBox2 = document.getElementById("userBox2");
 
 main.style.backgroundImage = " url(brilliant.jpg)";
 
 step1.addEventListener("click", () => {
   svgCircle.style.strokeDashoffset = "1256";
   main.style.backgroundImage = "url(neon.jpg)";
+  userBox2.style.top = "170px";
 });
 
 step2.addEventListener("click", () => {
-  svgCircle.style.strokeDashoffset = "960";
+  svgCircle.style.strokeDashoffset = "950";
   main.style.backgroundImage = "url(highrise.jpg)";
+  userBox2.style.top = "-300px";
 });
 
 step3.addEventListener("click", () => {
-  svgCircle.style.strokeDashoffset = "634";
+  svgCircle.style.strokeDashoffset = "645";
   main.style.backgroundImage = "url(differentDay.jpg)";
+  userBox2.style.top = "-730px";
 });
 step4.addEventListener("click", () => {
   svgCircle.style.strokeDashoffset = "0";
